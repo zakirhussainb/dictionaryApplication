60050f8d3929835da1762293074694ce5a4e09b0
Zakir Hussain <zakirhussainb2693@gmail.com>
2018-09-27 02:37:03 +0530

    Initial Commit

---
+++ src/main/java/com/zakcorp/esdictionary/EsdictionaryApplication.java
@@ -0,0 +1,13 @@
+package com.zakcorp.esdictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsdictionaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsdictionaryApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zakcorp/esdictionary/EsdictionaryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zakcorp.esdictionary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EsdictionaryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
